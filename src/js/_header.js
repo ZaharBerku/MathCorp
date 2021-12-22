d7c0e1c1393c5b8c5128d333a23d00806ea8bb7a
Nazar <zaharberku@gmail.com>
2021-12-23 01:13:34 +0200

    section-info-join

--- src/js/_header.js
+++ src/js/_header.js
@@ -3,12 +3,14 @@ const lineBtnBurger = document.querySelector('.section-navigation__burger-menu-l
 const menu = document.querySelector('.section-navigation__navigation-menu');
 
 let activeMenuBurger = (event) => {
-    if(btnBurger.classList.contains('btn-burger')){
+    if (event.currentTarget.classList.contains('btn-burger')) {
+        document.body.classList.remove('stop-scroll')
         menu.classList.remove('navigation-menu')
         lineBtnBurger.classList.remove('burger-menu-line')
-       return event.target.classList.remove('btn-burger')
+        return btnBurger.classList.remove('btn-burger')
     }
-    event.target.classList.add('btn-burger');
+    document.body.classList.add('stop-scroll')
+    btnBurger.classList.add('btn-burger');
     lineBtnBurger.classList.add('burger-menu-line')
     menu.classList.add('navigation-menu')
 }
