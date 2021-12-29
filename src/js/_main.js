8609d1dfe44e95be593df785e4ed9d2bc83ceb36
Nazar <zaharberku@gmail.com>
2021-12-29 19:53:41 +0200

    section-comment

--- src/js/_main.js
+++ src/js/_main.js
@@ -11,7 +11,86 @@ let hideTeacherInfo = () =>{
             })
 }
 
-hideTeacherBtn.addEventListener('click', hideTeacherInfo)
+hideTeacherBtn.addEventListener('click', hideTeacherInfo);
+
+
+//Slider
+
+
+const wrapperr = document.querySelector('.section-comment__sliders');
+const block = document.querySelector('.section-comment__wrapper');
+const slide = document.querySelectorAll('.section-comment__slide');
+
+
+let x;
+let startX;
+let pressed = false;
+let size = 0;
+
+
+let startDragMouse = (event => {
+    pressed = true;
+    startX = event.clientX;
+})
+
+block.addEventListener('mousedown', startDragMouse)
+
+let startDragTouch = (event => {
+    pressed = true;
+    startX = event.changedTouches[0].clientX;
+})
+
+
+block.addEventListener('touchstart', startDragTouch)
+
+let draggerMouse = (event) => {
+    if (!pressed) return
+    x = event.clientX - startX;
+    wrapperr.style.transform = `translate3d(${x + size}px, 0, 0)`
+
+    checkBoundary()
+}
+
+block.addEventListener('mousemove', draggerMouse)
+
+let draggerTouch = (event) => {
+    if (!pressed) return
+    x = event.changedTouches[0].clientX - startX;
+    wrapperr.style.transform = `translate3d(${x + size}px, 0, 0)`
+
+    // checkBoundary()
+}
+
+block.addEventListener('touchmove', draggerTouch)
+
+let endDrag = () => {
+    // const inner = wrapperr.getBoundingClientRect();
+    // const out = block.getBoundingClientRect();
+    pressed = false;
+    size = x + size;
+    // if (inner.right > inner.width) {
+    //     wrapperr.style.transform = `translate3d(0, 0, 0)`
+    // }
+    // if (inner.left  < -out.width) {
+    //     wrapperr.style.transform = `translate3d(-${out.width}px, 0, 0)`
+    // }
+}
+
+window.addEventListener('mouseup', endDrag)
+window.addEventListener('touchend', endDrag)
+
+function checkBoundary(){
+    const inner = wrapperr.getBoundingClientRect();
+    const out = block.getBoundingClientRect();
+    const different = 82;
+    if (inner.right - different > inner.width) {
+        wrapperr.style.transform = `translate3d(0, 0, 0)`
+    }
+    if (inner.left  < different - out.width) {
+        wrapperr.style.transform = `translate3d(-${out.width}px, 0, 0)`
+    }
+}
+
 
 
 
